cae8bb2667506c68774efb064f48480bf21fe2b0
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2023-11-05 02:43:29 +0100

    finalne izmjene

--- Zadatak 2/index.js
+++ Zadatak 2/index.js
@@ -1,22 +1,23 @@
-    let tableHtml = '<table>';
-    for (let i = 0; i <= 10; i++) {
-      tableHtml += '<tr>';
-      for (let j = 0; j <= 10; j++) {
-        if (i === 0 && j === 0) {
-          tableHtml += '<th id="header">X</th>';
-        } else if (i === 0) {
-          tableHtml += `<th>${j}</th>`;
-        } else if (j === 0) {
-          tableHtml += `<th>${i}</th>`;
-        } else {
-          tableHtml += `<td>${i * j}</td>`;
+let tabela = "<table>";
+for(let i = 0; i <= 10; i++){
+    tabela += "<tr>";
+    for(let j = 0; j <= 10; j++){
+        if(i == 0 && j == 0){
+            tabela += '<th id ="header">X</th>';
         }
-      }
-      tableHtml += '</tr>';
+        else if(i == 0){
+            tabela += `<th>${j}</th>`;
+        }
+        else if(j == 0){
+            tabela += `<th>${i}</th>`;
+        }
+        else{
+            tabela += `<td>${i * j}</td>`;
+        }
     }
-    tableHtml += '</table>';
-  
-    const tabelaDiv = document.getElementById('tabela');
-    tabelaDiv.innerHTML = tableHtml;
+    tabela += '</tr>';
+}
+
+const tabelaDiv = document.getElementById("tabela");
+tabelaDiv.innerHTML = tabela;
  
-  
